c3cc5e35b206886bef5f3ea5c6689b130017027a
eduardodiaspeixoto <62449665+eduardodiaspeixoto@users.noreply.github.com>
2020-03-20 20:53:20 -0300

    Update JoKenPo.py

--- JoKenPo.py
+++ JoKenPo.py
@@ -1,22 +1,48 @@
-#JokenPo
+def endMenu():
+    print('Escolher modo de jogo (PRESS 1)')
+    print('Jogar novamente no modo atual (PRESS 2)')
+    print('Encerrar programa (PRESS 0)')
+    escolha = int(input('>>Digite aqui : '))
 
-print('It\'s JO-KEN-PO Time!!\nNa sua vez escolha entre Pedra, Papel e Tesoura.')
+    if escolha == 1 :
+        menu()
+    elif escolha == 2 :
+        vsCpu()
+    elif escolha == 0 :
+        print ('Encerrando o jogo')
+        sleep(2)
+        print("Jogo encerrado. \nObrigado por jogar.")
+        0
+    else :
+        print ('Escolha invalida')
+        print ('TENTE NOVAMENTE')
+        endMenu()
+def vsCPU():
+    print('It\'s JO-KEN-PO Time!!\nNa sua vez escolha entre Pedra, Papel e Tesoura.')
 
-Jogador1 = input('Vez do Jogador nº1:')
-Jogador2 = input('Vez do Jogador nº2:')
-Pedra = str('Pedra')
-Papel = str('Papel')
-Tesoura = str('Tesoura')
+    Pedra = str('Pedra')
+    Papel = str('Papel')
+    Tesoura = str('Tesoura')
+
+    Jogador1 = input('Sua vez :')
+    Jogador2 = random.choice([Pedra,Papel,Tesoura])
+
+    print ('CPU jogou : ' , Jogador2)
 
 
-if (Jogador1 == Jogador2):
-    print('DRAW! Opa, temos um empate.Tente novamente!')
 
-if (Jogador1 == Pedra  and Jogador2 == Tesoura):
-    print('WE HAVE A WINNER! Parabéns ao Jogador nº1 pela vitória!!')
-if (Jogador1 == Papel  and Jogador2 == Pedra):
-    print('WE HAVE A WINNER! Parabéns ao Jogador nº1 pela vitória!!')
-if (Jogador1 == Tesoura  and Jogador2 == Papel):
-    print('WE HAVE A WINNER! Parabéns ao Jogador nº1 pela vitória!!')
-else:
-    print('WE HAVE A WINNER! Parabéns ao Jogador nº2 pela vitória!!')
+    if (Jogador1 == Jogador2):
+        print('DRAW! Opa, temos um empate.Tente novamente!')
+
+    if (Jogador1 == Pedra  and Jogador2 == Tesoura):
+        print('WE HAVE A WINNER! Parabéns ao Jogador nº1 pela vitória!!')
+    if (Jogador1 == Papel  and Jogador2 == Pedra):
+        print('WE HAVE A WINNER! Parabéns ao Jogador nº1 pela vitória!!')
+    if (Jogador1 == Tesoura  and Jogador2 == Papel):
+        print('WE HAVE A WINNER! Parabéns ao Jogador nº1 pela vitória!!')
+    else:
+        print('WE HAVE A WINNER! Parabéns ao Jogador nº2 pela vitória!!')
+
+    endMenu()
+
+    
